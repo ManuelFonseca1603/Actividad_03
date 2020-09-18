23154e456d672812f9c2ba7938676007228322bd
Manuel Fonseca <manuel.fonseca45499@alumnos.udg.mx>
2020-09-18 10:18:56 -0500

    Capturamos todo el nombre con ayuda de fgets

--- Tarea.cpp
+++ Tarea.cpp
@@ -3,7 +3,12 @@
 
 main ()
 {
+	char nombre[50];
+	
 	printf("Hola Mundo!!!\n");
+	printf("\nEscribe tu nombre: ");
+	fgets(nombre,sizeof(nombre),stdin);
+	printf("Hola %s\n\n",nombre);
 	
 	system("pause");
 }
