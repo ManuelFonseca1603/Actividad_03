2ee85e0635f4200fd5406eefcca371d138fb252d
Manuel Fonseca <manuel.fonseca45499@alumnos.udg.mx>
2020-09-18 10:46:13 -0500

    capturamos la edad e imprimimos los años que va a cumplir

--- Tarea.cpp
+++ Tarea.cpp
@@ -4,11 +4,17 @@
 main ()
 {
 	char nombre[50];
+	int edad;
 	
 	printf("Hola Mundo!!!\n");
 	printf("\nEscribe tu nombre: ");
 	fgets(nombre,sizeof(nombre),stdin);
-	printf("Hola %s\n\n",nombre);
+	printf("Hola %s\n",nombre);
+	printf("Escribe tu edad: ");
+	scanf("%i",&edad);
+	printf("Vas a cumplir %i",edad + 1);
 	
+	
+	printf("\n\n");
 	system("pause");
 }
